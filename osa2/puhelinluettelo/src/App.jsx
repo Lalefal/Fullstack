9d7ef7fa4b54d1972b0af10bdff40d143b45c29e
Lalefal <Laura.falenius@gmail.com>
2024-01-23 10:11:29 +0200

    tehtävä 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,55 @@
+//step1, henkilön lisäys luetteloon
+
+import { useState } from "react"
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    //luettelo
+    {
+      //id: 1,
+      name: "Arto Hellas",
+    },
+  ])
+  const [newName, setNewName] = useState("") //lomakkeen kentän kontrollointiin (input field)
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+     // id: persons.length + 1,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName("")
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {/* <div>debug: {newName}</div> */}
+
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}> {person.name} </p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
